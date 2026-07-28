54d4636baa256e68ac0bc7ab9bc2c220176c7abf
mitchellculverhouse-cloud <mitchellculverhouse@gmail.com>
2026-07-28 16:13:48 +0100

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,10 +1,10 @@
 SOURCES = [
 
     {
-        "name": "Aramco",
-        "type": "html",
-        "url": "https://careers.aramco.com/expat_uk/go/For-European-Candidates/7717923",
-        "active": True
-    }
+    "name": "WUZZUF",
+    "type": "rss",
+    "url": "https://wuzzuf.net/all-jobs.xml",
+    "active": True
+}
 
 ]
